a7adff3bed455f68851f2f8368d82b1f6c07fcdd
Ali-Amechghal <ali.amechghal@gmail.com>
2016-07-28 16:02:24 +0100

    hawk cli

--- hawk_cli/index.js
+++ hawk_cli/index.js
@@ -1,18 +1,41 @@
 var vorpal = require('vorpal')();
-console.log('============= Hawk =============');
+var path =  require('path');
 
+var configurationPaths =  require('./config/configPaths.json');
+
+console.log('\t\t\t$$$$$$$$$$$$$$$$$$$$$ Hawk $$$$$$$$$$$$$$$$$$$$$$');
+console.log('\t\t\t$$  Tool to Manage Hawk internal Processing  $$');
+console.log('\t\t\t$$  =========== CopyRights @Hawk ===========  $$');
 vorpal
-  .command('conf <name>', 'Outputs configuration files conf <configuration name>')
+  .command('secureConf <name>', 'Convert config file to secure format')
   .action(function(args, callback) {
-  	console.log(args);
-  	if(Object.keys(args.options).length < 1){
-
-  		console.log('provide configuration name  argument :');
-  		console.log('\t db : database');
-  		console.log('\t param : parameter');
-  	}
-
+    callback();
+  });
+  //initialize the referentiel
+  vorpal
+  .command('initRefTable <table_name> <data_file>', 'Initialize the referential table')
+  .action(function(args, callback) {
+    callback();
+  });
+  vorpal
+  .command('insertToRefTable <table_name> <data_file>', 'Insert  or append given data to the referential table')
+  .action(function(args, callback) {
+    callback();
+  });
+  vorpal
+  .command('updateRefTable <table_name> <data_ligne> <activate>', 'Activate/Deactivate MSISCN in the referential table')
+  .action(function(args, callback) {
+    callback();
+  });
+  vorpal
+  .command('truncateRefTable <table_name> <why>', 'truncate the referential table')
+  .action(function(args, callback) {
+    callback();
+  });
+  vorpal
+  .command('deleteFromRefTable <table_name> <key>', 'delete given Msisdn from the referential table')
+  .action(function(args, callback) {
     callback();
   });
 
-vorpal.delimiter('Hawk >').show();
+vorpal.delimiter('Hawk $').show();
